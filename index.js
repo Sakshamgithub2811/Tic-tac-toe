6a6c9cf4df629143e805ad70e4a104339aa09ce1
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2024-01-28 17:23:33 +0530

    90 % completed
    draw conditon is remaining

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;   // playerX,playerY
 
@@ -13,6 +16,13 @@ const winPatterns = [
     [3,4,5],
     [6,7,8],
 ];
+
+const resetGame = () =>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+}
 
 boxes.forEach((box)=>{
     box.addEventListener("click",() =>{
@@ -43,9 +53,32 @@ const checkWinner = () =>{
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText 
         if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
-            if(pos1Val == pos2Val && pos2Val == pos3Val)
+            if(pos1Val == pos2Val && pos2Val == pos3Val){
             console.log("winner condition",pos1Val);
-
+            showWinner(pos1Val);
+            disabledBoxes();
+            }
         }
     }
 }
+
+const showWinner = (winner)=>{
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const disabledBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
